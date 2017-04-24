9d70078d60d72ede4ec0db3d47354d153d658407
Jomar Dimaculangan <jomaridimaculangan@gmail.com>
2017-04-24 00:58:07 -0700

    Done.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,5 +1,5 @@
 ﻿namespace HW14_Dimaculangan {
-    partial class Form1 {
+    partial class TrieForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -23,12 +23,101 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent () {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.PrefixInputBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ResultBox = new System.Windows.Forms.TextBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.LoadButton);
+            this.groupBox1.Controls.Add(this.PrefixInputBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 9);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(340, 53);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Prefix Input";
+            // 
+            // PrefixInputBox
+            // 
+            this.PrefixInputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PrefixInputBox.Location = new System.Drawing.Point(6, 19);
+            this.PrefixInputBox.Name = "PrefixInputBox";
+            this.PrefixInputBox.Size = new System.Drawing.Size(262, 20);
+            this.PrefixInputBox.TabIndex = 0;
+            this.PrefixInputBox.TextChanged += new System.EventHandler(this.PrefixInputBox_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ResultBox);
+            this.groupBox2.Location = new System.Drawing.Point(12, 68);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(340, 390);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // ResultBox
+            // 
+            this.ResultBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultBox.Location = new System.Drawing.Point(1, 20);
+            this.ResultBox.Multiline = true;
+            this.ResultBox.Name = "ResultBox";
+            this.ResultBox.ReadOnly = true;
+            this.ResultBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ResultBox.Size = new System.Drawing.Size(333, 364);
+            this.ResultBox.TabIndex = 0;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadButton.Location = new System.Drawing.Point(258, 19);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(75, 23);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            // 
+            // TrieForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(364, 470);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "TrieForm";
+            this.Text = "Trie Auto-Complete Jomar Dimaculangan";
+            this.Load += new System.EventHandler(this.TrieForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox PrefixInputBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox ResultBox;
+        private System.Windows.Forms.Button LoadButton;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,15 +3,177 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HW14_Dimaculangan {
-    public partial class Form1 : Form {
-        public Form1 () {
+    public partial class TrieForm : Form {
+        public TrieForm () {
             InitializeComponent();
+
+            Root.BuildDictTrie();
+        }
+
+        private void TrieForm_Load (object sender, EventArgs e) {
+            
+        }
+
+        public static string [] ArrDict => File.ReadAllLines("wordsEn.txt");
+
+        public Trie Root = new Trie();
+        //now let's build the trienodes wit childs:
+        public class TrieNode {
+            public char C;
+            public List<TrieNode> Child = new List<TrieNode>();
+            //constructors:
+            public TrieNode() {
+                C = new char();
+                Child = new List<TrieNode>();
+            }
+
+            public TrieNode(char newc) {
+                C = newc;
+               
+            }
+
+            //courtesy of evan olds: 
+            public TrieNode AddOrGet(char childCharacter) {
+                //go through each child and search all of it's characters. if its in none of em, its done 
+                foreach (TrieNode t in Child) {
+                    //
+                    if (t.C == childCharacter) {
+                        return t;
+                    }
+                }
+                var n = new TrieNode(childCharacter);
+                Child.Add(n);
+                return n;
+            }
+
+            //find where the node is:
+            public TrieNode FindChildNode(char cc) {
+                foreach (var node in Child) {
+                    if (node.C == cc) {
+                        return node;
+                    }
+                }
+                return null;
+            }
+            
+        }
+
+        //now lets build the Trie data structure:
+        public class Trie {
+            private readonly TrieNode _root = new TrieNode('\0');
+            //public TrieNode CurNode = new TrieNode();
+            public List<string> prefixes = new List<string>();
+            public string prefix = null;
+
+            public void addString(string S) {
+                TrieNode n = new TrieNode();
+                n = _root;
+                foreach (char sc in S) {
+                    n = n.AddOrGet(sc);
+                }
+                n = n.AddOrGet('\0');
+            }
+
+            //load the dictionary to a trei:
+            public void BuildDictTrie() {
+                foreach (string s in ArrDict) {
+                    //build the trie by adding all strings:
+                    addString(s);
+                }
+            }
+
+            
+            //grabs the prefixNode:
+            public TrieNode PrefixNode(string s) {
+                
+                //Have a temporary prefix node variable and we always starting at root
+                var prefNode = _root;
+                //res will update everytime we go through the array
+                var res = prefNode;
+                foreach (var c in s) {
+                    prefNode = prefNode.FindChildNode(c);
+                    //if the next one is null, just end it and return the prior one
+                    if (prefNode == null) {
+                        break;
+                    }
+                    //prefix += c;
+                    res = prefNode;
+                }
+                return res;
+            }
+
+            public void getPrefixes(string origs, TrieNode node) {
+                //clear lists:
+                prefixes.Clear();
+                //this kills the program:
+                if (string.IsNullOrEmpty(origs)) {
+                    return;
+                }
+                foreach (var n in node.Child) {
+                    BuildPrefix(origs,n.C + "", n);
+                }
+            }
+            //grabs the string from input box and returns all the matching prefix
+            public void BuildPrefix(string origs, string s, TrieNode childNode) {
+                //we will be adding a suffixing string:
+                //if the one has nothing in it, return;
+                
+                
+                if (childNode.C == '\0') {
+                    var fullword = origs + s;
+                    prefixes.Add(fullword.Substring(0,fullword.Length-1));
+                    return;
+                }
+                //go through each child:
+                foreach (TrieNode cnode in childNode.Child) {
+                    //go through each until they find a '\0; 
+                    //add that character until that '\0 is found;
+                        BuildPrefix(origs,s+cnode.C,cnode);
+                }
+            }
+        }
+
+        public void disableResultBox() {
+            ResultBox.Enabled = false;
+        }
+
+        private void PrefixInputBox_TextChanged (object sender, EventArgs e) {
+            
+        }
+
+        private void LoadButton_Click (object sender, EventArgs e) {
+            //disable result box and clear it.
+            disableResultBox();
+            ResultBox.Clear();
+            Root.prefixes.Clear();
+            //hella slow so i created threads for functions:
+            //ThreadStart getPrefs =
+            //    delegate { Root.getPrefixes(PrefixInputBox.Text, Root.PrefixNode(PrefixInputBox.Text)); };
+            //Thread preffixThread = new Thread(getPrefs);
+            Root.getPrefixes(PrefixInputBox.Text, Root.PrefixNode(PrefixInputBox.Text));
+            ThreadStart resultboxthreadstart = delegate {
+                foreach (string s in Root.prefixes) {
+                    ResultBox.Text += s + "\r\n";
+                }
+                IAsyncResult res = this.BeginInvoke(new Action(delegate { ResultBox.Enabled = true; }));
+            };
+            Thread resultThread = new Thread(resultboxthreadstart);
+            resultThread.Start();
+
+            //Root.getPrefixes(PrefixInputBox.Text, Root.PrefixNode(PrefixInputBox.Text));
+            //foreach (string s in Root.prefixes) {
+            //    ResultBox.Text += s + "\r\n";
+            //}
         }
     }
 }
